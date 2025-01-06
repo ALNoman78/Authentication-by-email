8141adf203c91b8b8c7241397f8ff2be0a835082
ALNoman78 <abdullaalno078@gmail.com>
2025-01-06 23:30:17 +0600

    authentication by email and password

---
+++ src/__auth__.js
@@ -0,0 +1,4 @@
+/**
+ * 1. Step - 1: login === signin => you're the old user , you already have the credential (email and password to login)
+ * 2. Step - 2: Register === signup === create account => you are a brand new user , new never this website before . you don't have email password that have access
+*/
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Home = props => {
+    return (
+        <div>
+            <h3 className='text-3xl font-bold text-center'>Welcome to email and password authentication</h3>
+        </div>
+    )
+}
+
+Home.propTypes = {}
+
+export default Home

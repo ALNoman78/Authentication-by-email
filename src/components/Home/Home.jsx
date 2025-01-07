8fd63c2c4a72c3b2aec9818e5dae264c7e71ac99
ALNoman78 <abdullaalno078@gmail.com>
2025-01-07 12:06:20 +0600

    h2 added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ const Home = props => {
     return (
         <div>
             <h3 className='text-3xl font-bold text-center'>Welcome to email and password authentication</h3>
+            <h2>tois</h2>
         </div>
     )
 }

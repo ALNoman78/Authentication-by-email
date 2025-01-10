a4b480c7eb94e9e0edc821f78eda3f2e016cd762
ALNoman78 <abdullaalno078@gmail.com>
2025-01-10 19:58:15 +0600

    remove changeges

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 const Login = props => {
     return (
-        <div>Login
-             <i>This is Labonno</i>
+        <div>
+            Login
         </div>
        
     )

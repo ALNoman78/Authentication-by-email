93285427c3a21dc58e2bb9e8acbabe412d7381e7
ALNoman78 <abdullaalno078@gmail.com>
2025-01-16 12:07:03 +0600

    passsword eye button added

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import auth from '../../firebase/firebase.init'
+// import PasswordInput from '../PasswordInput/PasswordInput'
+import { Eye, EyeOff } from 'lucide-react';
 
 const SignUp = () => {
     const [user, setUser] = useState(null)
-    const [success , setSuccess] = useState(false)
+    const [success, setSuccess] = useState(false)
     const [error, setError] = useState('')
+    const [isVisible, setIsVisible] = useState(false);
 
     const handleSignUp = e => {
         e.preventDefault()
@@ -16,7 +19,7 @@ const SignUp = () => {
 
         // password validation
 
-        if(password.length < 6){
+        if (password.length < 6) {
             setError('Password should be 6 character or longer');
             return;
         }
@@ -25,7 +28,7 @@ const SignUp = () => {
 
         const passwordRegex = /^(?=.*[a-z]) (?=.*[A-Z]) (?=.*\d) (?=.*[@$!%*?&]) [A-Za-z\d@$!%*?&]{6,}$/;
 
-        if(!passwordRegex.test(password)){
+        if (!passwordRegex.test(password)) {
             setError('At least one upperCase , one lowercase , one number , one special character')
             return;
         }
@@ -35,7 +38,7 @@ const SignUp = () => {
         setSuccess(false)
 
         // sing in with authentication 
-        createUserWithEmailAndPassword(auth , email , password)
+        createUserWithEmailAndPassword(auth, email, password)
             .then((result) => {
                 setUser(result.user)
                 // success message
@@ -62,10 +65,33 @@ const SignUp = () => {
                             <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            {/* <PasswordInput></PasswordInput> */}
+                            {/* <input type="password" name='password' placeholder="password" className="input input-bordered" required /> */}
+
+                            {/* start password input */}
+                            <div className="w-full mx-auto">
+                                <label htmlFor="pass" className="text-sm font-normal">
+                                    Password
+                                </label>
+                                <div className="relative mt-1">
+                                    <input
+                                        type={isVisible ? 'text' : 'password'}
+                                        id="pass"
+                                        placeholder="Password"
+                                        name='password'
+                                        className="bg-background w-full outline-none focus-within:border-blue-700 rounded-md p-2 border-2"
+                                    />
+                                    <div
+                                        className="absolute top-3 right-4 text-2xl text-gray-500 cursor-pointer"
+                                        onClick={() => setIsVisible((prev) => !prev)}
+                                    >
+                                        {isVisible ? <Eye size={22} /> : <EyeOff size={22} />}
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* password input */}
+
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -73,16 +99,16 @@ const SignUp = () => {
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Sign up</button>
                         </div>
-                            {
-                                error && <p className='text-center font-medium text-red-600'>
-                                    {
-                                        error
-                                    }
-                                </p>
-                            }
-                            {
-                                success && <p className='text-green-600 text-center font-medium'>Sign Up is Success</p>
-                            }
+                        {
+                            error && <p className='text-center font-medium text-red-600'>
+                                {
+                                    error
+                                }
+                            </p>
+                        }
+                        {
+                            success && <p className='text-green-600 text-center font-medium'>Sign Up is Success</p>
+                        }
                     </form>
                 </div>
             </div>

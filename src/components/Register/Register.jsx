b2d6966c3ea4d767ac550b1f76bea9cb40d43ac3
ALNoman78 <abdullaalno078@gmail.com>
2025-01-31 23:53:09 +0600

    authentication user

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -16,9 +16,10 @@ const Register = () => {
         e.preventDefault()
         const email = e.target.email.value
         const password = e.target.password.value
+        const username = e.target.username.value
         // terms and policy section
         const terms = e.target.terms.checked
-        console.log(email, password)
+        console.log(email, password , username)
 
         // checked mark sign
         if (!terms) {
@@ -47,7 +48,7 @@ const Register = () => {
 
                 // send verification email address
                 sendEmailVerification(auth.currentUser)
-                .then(res => console.log('verification email send'))
+                    .then(res => console.log('verification email send'))
             })
             .catch(error => {
                 setError(error.message)
@@ -57,6 +58,17 @@ const Register = () => {
     return (
         <div className='max-w-lg mx-auto p-4'>
             <form onSubmit={handleRegister} className=''>
+                <label className="input input-bordered flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                    <input type="text" className="grow" name='username' placeholder="Username" />
+                </label>
                 <label className="input input-bordered flex items-center gap-2 my-5">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -92,14 +104,15 @@ const Register = () => {
                             d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                             clipRule="evenodd" />
                     </svg>
-                    <div>
+
+                    <div className='relative'>
                         <input
                             type={isVisible ? 'text' : 'password'} className="grow w-[90%]"
                             placeholder='Password'
                             name='password' required />
                         <button
                             onClick={() => setIsVisible(!isVisible)}
-                            className='btn btn-xs'>
+                            className='btn btn-xs absolute -right-[14.5rem]'>
                             {
                                 isVisible ? <FaEye /> : <FaEyeSlash />
                             }
